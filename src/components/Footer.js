4b28c16590f420dd6dbacdf5c08081c0800e420e
Vishant <iamvishant01@gmail.com>
2025-02-10 12:47:40 +0530

    Footer improvements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,19 @@ export default function Footer() {
     <div classNameName="footer-main">
         <footer className="footer">
             <div className="footer-col">
-                <h3>Stay Connected Forever !</h3>
-                <p>Some content here.</p>
+                <h3 id='footer-heading'>Evergreen Writes</h3>
+                <br/>
+                <a href="https://iamvishant.blogspot.com/2025/02/vishant-netke-journey-of-innovation.html" target='about_blank'><p id='posts-footer'>Vishant Netke: The Journey of Innovation, Growth, and Storytelling</p></a>
+                <a href="/"><p id="posts-footer">The Astrology : Unveiling the effect of Stars and Destiny</p></a>
+            </div>
+            <div className="footer-col">
+              <h3 id='footer-heading'>Stay in Touch on Wildr</h3>
+              <br/>
+              <a href="https://www.linkedin.com/in/iamvishant01/" target='about_blank'><p id="posts-footer"><i class="fa-brands fa-linkedin"></i> Linkedin</p></a>
+              <a href="https://www.instagram.com/iamvishant01" target='about_blank'><p id="posts-footer"><i class="fa-brands fa-square-instagram"></i> Instagram</p></a>
+              <a href="https://github.com/Iamvishant01" target='about_blank'><p id="posts-footer"><i class="fa-brands fa-github"></i> Github</p></a>
+              <a href="https://www.reddit.com/user/Unique_Plastic_4202/" target='about_blank'><p id="posts-footer"><i class="fa-brands fa-reddit"></i> Reddit</p></a>
+              <a href="mailto:iamvishant01@gmail.com"><p id="posts-footer"><i class="fa-solid fa-envelope"></i> Mail</p></a>
             </div>
         </footer>
     </div>
