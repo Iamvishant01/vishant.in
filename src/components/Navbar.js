62f2dbaa154cb0f8a3bd8c2c244edff50436319c
Vishant netke <112625850+Iamvishant01@users.noreply.github.com>
2025-02-21 00:08:34 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ export default function Navbar() {
           </ul>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-dark" type="submit">Search</button>
+            <button className="btn btn-dark" type="submit" onClick={handleAlert} >Search</button>
           </form>
         </div>
       </div>
