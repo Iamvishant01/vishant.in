18baf6481d47188d20ca14560d47efaf78538c5f
Vishant <iamvishant01@gmail.com>
2025-03-28 23:36:58 +0530

    Blog Added

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -16,6 +16,31 @@ const Blogs = forwardRef((props, ref) => {
 
       <div className="posts">
 
+        {/* post 4 */}
+      <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              sx={{ width: "100%", height: "140px" }}
+              image="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi85-XxATlVnePts0dLZINFEuZOaOWdO4tEBlQvGsQeNG5H459H_nnRKC9a4-I0NTydYJKA7iT_sE41kPr6dJi3hhCdYiHsmkVylTpesyLWjS16c8iQSwIW_xZhAW6NwmMulP31PKW1MblNaM48KuYK2ou9qO6C_vPyCgRtFRXrcvQjBQb578I1kl7Cdmi9/s16000/blog2.webp"
+              alt="Galaxy"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5">
+              Footsteps of Wisdom: Lessons from My Journey
+              </Typography>
+              <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Back in January, I went on an exciting trip to Nashik with my friends. We had been planning it for a while, even before our exams wrapped up. To make the journey even more fun, we decided to take the train ...
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <a href="https://iamvishant.blogspot.com/2025/03/footsteps-of-wisdom-lessons-from-my.html" target="_blank" rel="noopener noreferrer">
+              <Button size="small" color="primary">Read More</Button>
+            </a>
+          </CardActions>
+        </Card>
+
                 {/* post 3 */}
         <Card sx={{ maxWidth: 345 }}>
           <CardActionArea>
